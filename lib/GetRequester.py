d146f97ec5e31cab36bad701c9895e55381f74d9
Joshua <joshua.gatune@student.moringaschool.com>
2023-09-20 13:35:56 +0300

    initial

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,3 +1,5 @@
+from urllib import response
+from flask import Response
 import requests
 import json
 
@@ -8,6 +10,23 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+    # Send GET request to URL and get response.
+        response = requests.get(self.url)
+
+    # Check if request was succesfull
+    if response.status_code == 200:
+        return response.text # return response body content as a str    
+    else:
+        raise Exception(f"failed with status code {response.status_code}")
+        
 
     def load_json(self):
         pass
+    # Get response body as a string using the get_response_body method
+        response_body = self.get_response_body()
+
+    # Load JSON from response body into Python object
+        json_data = json.loads(response_body)
+
+        # Return the Python data
+        return json_data 
